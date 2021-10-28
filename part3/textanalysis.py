20f754754c0cc87217e850890c0ecd1f6ffb5a79
UNILTX <tli2@babson.edu>
2021-10-27 20:36:12 -0400

    Update textanalysis.py

--- part3/textanalysis.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-
-url = 'https://www.gutenberg.org/files/730/730-0.txt'
-response = urllib.request.urlopen(url)
-data = response.read()  # a `bytes` object
-text = data.decode('utf-8')
-print(text) 
